a6fdf086b99294cb00edb0691d61d20bf0cf2b7a
jcbain <jamescbain@gmail.com>
2020-04-09 00:16:01 -0600

    create test suite for DataHelpers.js

--- src/helpers/DataHelpers.test.js
+++ src/helpers/DataHelpers.test.js
@@ -1,6 +1,16 @@
-import {returnRelevantParams} from './DataHelpers';
+import {unique, returnRelevantParams, filterDataByParams} from './DataHelpers';
 
-test ('this something', () => {
-    expect(returnRelevantParams([{hanes: 'hello', name: 'james'}, {item: 'name', name: 'jennifer', blog: 'somethin'}], {hanes: 'hello', name: 'james', blog: 'yes'})).toStrictEqual({hanes: 'hello', name: 'james', blog: 'yes'})
-})
-// returnRelevantParams([{hanes: 'hello', name: 'james'}, {item: 'name', name: 'jennifer', blog: 'somethin'}], {hanes: 'hello', name: 'james', blog: 'yes'})
+const tmpdata = [{name: 'jennifer', age: 29}, {name: 'james', age: 29}, {name: 'james', age: 29, hair: 'brown'}];
+const selectedParams = {name: 'james', age: 29, badparam: 'real bad'};
+
+test('unique [1, 2, 3, 3] should return [1, 2, 3]', () => {
+    expect([1, 2, 3, 3].filter(unique)).toStrictEqual([1, 2, 3]);
+});
+
+test ('returnRelevantParams() should return only params with keys in data', () => {
+    expect(returnRelevantParams(tmpdata, selectedParams)).toStrictEqual({name: 'james', age: 29});
+});
+
+test('filterDataByParams() should return data where parameters are exactly equal to a specific value', () => {
+    expect(filterDataByParams(tmpdata, selectedParams)).toStrictEqual([{name: 'james', age: 29}, {name: 'james', age: 29, hair: 'brown'}]);
+});
