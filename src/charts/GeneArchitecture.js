1fd65f22261537125322be35b8557bb60e10bc5d
jcbain <jamescbain@gmail.com>
2020-04-14 10:56:00 -0600

    include updating and filtering options

--- src/charts/GeneArchitecture.js
+++ src/charts/GeneArchitecture.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { select } from 'd3-selection';
 
+import { removeParams, filterDataByParams} from '../helpers/DataHelpers';
+
 
 class GeneArchitecture extends Component {
     constructor(props){
         super(props);
+        this.params = removeParams(this.props.params, ['output_gen', 'pop']);
+        this.filteredData = filterDataByParams(this.props.data, this.params)
+        this.genWidth = 5;
+
 
     }
 
     componentDidMount(){
-        console.log(this.props.data)
+        console.log(this.filteredData)
     }
 
 
