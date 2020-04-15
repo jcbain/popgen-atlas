5c8a98eda407e90363e683ec9e4f1bf796a5c428
jcbain <jamescbain@gmail.com>
2020-04-15 16:18:53 -0600

    adjust xscale for generation

--- src/charts/GeneArchitecture.js
+++ src/charts/GeneArchitecture.js
@@ -4,6 +4,7 @@ import { scaleLinear } from 'd3-scale';
 import { min, max } from 'd3-array';
 
 import { removeParams, filterDataByParams, leftJoinByAttr} from '../helpers/DataHelpers';
+import { thresholdFreedmanDiaconis } from 'd3';
 
 
 class GeneArchitecture extends Component {
@@ -13,14 +14,26 @@ class GeneArchitecture extends Component {
         this.params = removeParams(this.props.params, ['output_gen', 'pop']);
         this.data = leftJoinByAttr(filterDataByParams(this.props.data, this.params),this.props.template, ['position'], {positional_map: 'ind'})
         this.genWidth = 5;
-        this.xScale = scaleLinear().range([min(this.data, (d, i) => i), max(this.data, (d, i) => i)]).range([0, this.props.width]);
-        this.yScale = scaleLinear().range([min(this.props.template, d => d.ind), max(this.props.template, d => d.ind)]).domain([0, this.props.height])
+        this.xScale = scaleLinear().domain([min(this.data, d => d.output_gen), max(this.data, d => d.output_gen)]).range([0, this.props.width - this.genWidth]);
+        this.yScale = scaleLinear().domain([min(this.props.template, d => d.ind), max(this.props.template, d => d.ind)]).range([0, this.props.height])
 
 
     }
+    archRef = React.createRef();
 
     componentDidMount(){
-        console.log(this)
+       console.log( this.data.filter(d => d.pop === 0))
+       console.log(this.xScale(50000))
+        // select(this.archRef.current)
+        //     .selectAll('.genome-cross')
+        //     .data(this.data)
+        //     .enter()
+        //     .append('rect')
+        //     .attr('x', (d, i) => this.xScale(i))
+        //     .attr('y', 0)
+        //     .attr('height', this.props.height)
+        //     .attr('width', this.xScale(this.genWidth))
+
 
     }
 
@@ -28,7 +41,8 @@ class GeneArchitecture extends Component {
     render(){
 
         return(
-            <svg viewBox={[0, 0, this.props.width, this.props.height]}></svg>
+            <svg viewBox={[0, 0, this.props.width, this.props.height]}
+                 ref={this.archRef}></svg>
         )
     }
 }
