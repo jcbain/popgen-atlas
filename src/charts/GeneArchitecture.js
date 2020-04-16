53f2172f9efa319a564a99d10d44bee0045e1aa1
jcbain <jamescbain@gmail.com>
2020-04-16 12:05:35 -0600

    create a heat map with linear gradients

--- src/charts/GeneArchitecture.js
+++ src/charts/GeneArchitecture.js
@@ -4,7 +4,7 @@ import { scaleLinear } from 'd3-scale';
 import { min, max } from 'd3-array';
 
 import { unique, removeParams, filterDataByParams, leftJoinByAttr} from '../helpers/DataHelpers';
-import { thresholdFreedmanDiaconis } from 'd3';
+import { interpolateHcl } from 'd3-interpolate';
 
 
 class GeneArchitecture extends Component {
@@ -12,18 +12,21 @@ class GeneArchitecture extends Component {
         super(props);
         this.props.template.forEach((v,i) => v.ind = i);
         this.params = removeParams(this.props.params, ['output_gen', 'pop']);
-        this.data = leftJoinByAttr(filterDataByParams(this.props.data, this.params),this.props.template, ['position'], {positional_map: 'ind'})
+        this.data = leftJoinByAttr(filterDataByParams(this.props.data, this.params),this.props.template, ['position'], {positional_map: 'ind'}).filter(d => d.pop === 1);
         this.generations = this.data.map(d => d.output_gen).filter(unique);
         this.genWidth = this.props.width/this.generations.length;
         this.xScale = scaleLinear().domain([min(this.data, d => d.output_gen), max(this.data, d => d.output_gen)]).range([0, this.props.width - this.genWidth]);
-        this.yScale = scaleLinear().domain([min(this.props.template, d => d.ind), max(this.props.template, d => d.ind)]).range([0, this.props.height])
-
+        this.yScale = scaleLinear().domain([min(this.props.template, d => d.ind), max(this.props.template, d => d.ind)]).range([0, 100])
+        this.colorScale = scaleLinear()
+        .domain([min(this.props.data, d => d.positional_phen), 0, max(this.props.data, d => d.positional_phen)])
+            .range(['#C38D9E', '#fffff7', '#E27D60'])
+            .interpolate(interpolateHcl);
 
     }
     archRef = React.createRef();
 
     componentDidMount(){
-       console.log( this.data.filter(d => d.pop === 0))
+       console.log( this.data.filter(d => d.output_gen === 50000))
        console.log(this.xScale(50000))
        console.log(this.generations)
        select(this.archRef.current)
@@ -35,7 +38,7 @@ class GeneArchitecture extends Component {
             .attr('y',  0)
             .attr('width', this.genWidth)
             .attr('height', this.props.height)
-            .attr('fill', '#fffff7')
+            .attr('fill', d => `url(#gen-grad-${d})`)
         // select(this.archRef.current)
         //     .selectAll('.genome-cross')
         //     .data(this.data)
@@ -51,11 +54,43 @@ class GeneArchitecture extends Component {
 
 
     render(){
+
+        function SingleGrandient(props){
+            const selectedData = props.data.filter(d => d.output_gen === props.gen)
+            console.log(selectedData)
+            const gradient = selectedData.map( d =>
+                <stop key={`stop-gen-${props.gen}-ind-${d.positional_map}`}
+                      stopColor={props.colorScale(d.positional_phen)}
+                      offset={props.yScale(d.positional_map) + "%"}>
+                </stop>
+            )
+            return gradient;
+        }
+
+        const gradients = this.generations
+            .map( d => <linearGradient key={`gen-grad-${d}-yes`}
+                            gradientUnits='userSpaceOnUse'
+                            id={`gen-grad-${d}`}
+                            x1={0}
+                            x2={0}
+                            y1={0}
+                            y2={this.props.height}>
+                <SingleGrandient data={this.data} gen={d} colorScale={this.colorScale} yScale={this.yScale}></SingleGrandient>
+
+                
+
+            </linearGradient>)
         return(
             <svg viewBox={[0, 0, this.props.width, this.props.height]}
-                 ref={this.archRef}></svg>
+                 ref={this.archRef}>
+                {gradients}
+
+            </svg>
         )
     }
 }
+
+
+
 
 export default GeneArchitecture;
