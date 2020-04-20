1870156dd4f86f333703f34ada4bed3dc2b9c329
jcbain <jamescbain@gmail.com>
2020-04-20 12:28:24 -0600

    add testing script for helpers

---
+++ src/helpers/Helpers.test.js
@@ -0,0 +1,12 @@
+import {closestFromArray} from './Helpers';
+
+const tmpdata = [1, 100, 1000];
+const interval = closestFromArray(tmpdata);
+
+test('The return function of closestFromArray should return 100 when passed 99', () => {
+    expect(interval(99)).toBe(100);
+})
+
+test('The return function should round down if less than half way to next nearest value', () => {
+    expect(interval(49.9)).toBe(1);
+})
