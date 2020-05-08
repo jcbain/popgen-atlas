bb371987deb1ea2262ec77ed1c9f56d7a89515fa
jcbain <jamescbain@gmail.com>
2020-05-08 00:08:59 -0600

    dynamically generate parameter passing functions

--- src/components/ParameterCollection.js
+++ src/components/ParameterCollection.js
@@ -6,26 +6,34 @@ class ParameterCollection extends Component{
     constructor(props){
         super(props);
         this.onSelection = this.onSelection.bind(this);
-        this.state = {item: 0}
+        this.state = {item: 0, migration: 0}
     }
 
     onSelection(d){
         this.setState({item: d})
     }
+
     
     componentDidUpdate(){
         console.log(this.state);
     }
 
     render(){
+        let functObj = {}
+        Object.keys(this.props.labels).map(k => (
+            functObj[k] = (d) => this.setState({k: d})
+        ))
         const paramSelections = Object.keys(this.props.labels).map(k => (
             <ParameterSet key={k} 
                 label={k} 
                 options={this.props.data.map(d => d[this.props.labels[k]]).filter(unique)}
                 minWidth={120}
-                changeSelection={this.onSelection}></ParameterSet>
+                changeSelection={functObj[k].bind(this)}></ParameterSet>
             )
         )
+
+
+        console.log(functObj)
         return(
             <div>
                 {paramSelections}
