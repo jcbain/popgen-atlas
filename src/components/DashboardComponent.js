d93e7dc46afd12eb292e0c45a2d93f683d79caa8
jcbain <jamescbain@gmail.com>
2020-05-12 11:30:07 -0600

    get components showing in dashboard

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-
+import LineChartGroup from './LineChartGroup';
+import GeneArchGroup from './GeneArchGroup'
 
 import ChartLister from './ChartLister'
+import LineChart from '../charts/LineChart';
 
 class DashboardComponent extends Component{
     constructor(props){
         super(props);
         this.handleClick = this.handleClick.bind(this)
         this.state = {componentView: false,
-                      selectedComponent: ''};
+                      selectedComponent: '',
+                      params: {mu: '1e-6', m: '1e-4', r: '1e-6' , sigsqr: '25', output_gen: 1000, pop: 0}
+                    };
     }
 
     handleClick(d){
@@ -17,10 +21,21 @@ class DashboardComponent extends Component{
     }
 
     render(){
+        const charts = {
+            geneArchGroup: <GeneArchGroup data={this.props.data}
+                            template={this.props.template}
+                            params={this.state.params}
+                            useLocalParams={false}>
+            </GeneArchGroup>,
+            lineChartGroup: <LineChartGroup data={this.props.dataPopPhen}
+                                            params={this.state.params}
+                                            useLocalParams={false}></LineChartGroup>
+        }
+
         let display;
         if(this.state.componentView){
             display = <div>
-                <h1>Hello</h1>
+                {charts['lineChartGroup']}
                 <Button onClick={() => this.setState({componentView: false})} size="small">Remove</Button></div>
         } else {
             display = <ChartLister clickAction={this.handleClick}></ChartLister>
