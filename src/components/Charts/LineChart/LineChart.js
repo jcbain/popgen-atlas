4526e9a552787e6d5c78133e635902b6a28f026d
jcbain <jamescbain@gmail.com>
2020-07-22 15:21:29 -0600

    add padding to line chart

--- src/components/Charts/LineChart/LineChart.js
+++ src/components/Charts/LineChart/LineChart.js
@@ -10,14 +10,6 @@ import { FocusedStop, OutsideStop } from './LineChartStyles';
 import { closestFromArray } from '../../../helpers/Helpers';
 import { NativeSelect } from '@material-ui/core';
 
-// const FocusedStop = styled.stop`
-//     stop-color: #fcba03;
-// `
-
-// const OutsideStop = styled.stop`
-//     stop-color: #d6d6d6;
-// `
-
 const themePop0 = {
     popColorFocus: '#eb348f',
     popColorOutside: '#d6d6d6'
@@ -36,34 +28,17 @@ const themes = {
 const LineChart = (props) => {
     const { className, data, xDomain, 
             nestedVar, xVar, yVar, popStrokeWidth,
-            width, height, displayDims } = props;
+            width, height, displayDims, chartPadding } = props;
     const minY = min(data.map(d => min(d[nestedVar], v => v[yVar]))),
           maxY = max(data.map(d => max(d[nestedVar], v => v[yVar])));
-    const xScale = scaleLinear().domain(xDomain).range([0, width]),
-          yScale = scaleLinear().domain([maxY, minY]).range([0, height]);
+    const xScale = scaleLinear().domain(xDomain).range([chartPadding.left, width - chartPadding.right]),
+          yScale = scaleLinear().domain([maxY, minY]).range([chartPadding.top, height - chartPadding.bottom]);
     const drawLine = line().x(d => xScale(d[xVar])).y(d => yScale(d[yVar])).curve(curveMonotoneX);
     
-    // const gradient = (
-    //     <linearGradient id="tmp-lineargradient"
-    //         x1={0} x2={width}
-    //         y1={0} y2={0}
-    //         gradientUnits={'userSpaceOnUse'}
-    //     >
-    //         <ThemeProvider theme={themePop0}>
-    //             <OutsideStop offset={`20%`}></OutsideStop>
-    //             <FocusedStop offset={`20%`}></FocusedStop>
-    //             <FocusedStop offset={`80%`}></FocusedStop>
-    //             <OutsideStop offset={`80%`}></OutsideStop>
-    //         </ThemeProvider>
-    //     </linearGradient>
-    // )
-
-    console.log(data)
-
     const gradients = data.map((d, i) => (
         <linearGradient key={i}
             id={`tmp-lineargradient-${d.key}`}
-            x1={0} x2={width}
+            x1={chartPadding.left} x2={width - chartPadding.right}
             y1={0} y2={0}
             gradientUnits={'userSpaceOnUse'}
         >
@@ -86,8 +61,6 @@ const LineChart = (props) => {
             d={drawLine(d[nestedVar])}/>
 
     ))
-
-
     
     return (
         <svg className={className}
@@ -96,7 +69,6 @@ const LineChart = (props) => {
             height={`${displayDims.height}vh`}>
             {gradients}
             {lines}
-
         </svg>
     )
 
@@ -107,6 +79,7 @@ LineChart.defaultProps = {
     width: 500,
     height: 250,
     displayDims: {width: 100, height: 50},
+    chartPadding: {left: 20, right: 20, top: 10, bottom: 40},
     popStrokeWidth: 5,
 }
 
