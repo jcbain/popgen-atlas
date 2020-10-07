9e2e98423323d9822fe1d1eecae1e35b09669128
jcbain <jamescbain@gmail.com>
2020-10-07 15:37:59 -0600

    successfully set up react spring to be triggered by scroll trigger

--- src/components/Articles/LocalAdaptationArticle/Map/MapShapes.js
+++ src/components/Articles/LocalAdaptationArticle/Map/MapShapes.js
@@ -1,4 +1,5 @@
-import React, { useEffect, forwardRef } from 'react';
+import React, { useEffect, forwardRef, useState } from 'react';
+import { useSpring, animated } from 'react-spring'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -7,7 +8,10 @@ gsap.registerPlugin(ScrollTrigger);
 
 const MapShapes = forwardRef((props, ref) => {
     const { data, path, trigger, displayMarkers } = props;
+    const [ color, setColor ] = useState(props.fill || 'green')
+    const [ toggle, setToggle ] = useState(false)
 
+    const animatedProps = useSpring({opacity: toggle ? 0.3 : 0.9})
     useEffect(() => {
         if(ref) {
             gsap.to(ref.current, {
@@ -16,19 +20,23 @@ const MapShapes = forwardRef((props, ref) => {
                     markers: displayMarkers,
                     start: "top 80%",
                     end: "top 15%",
-                    toggleActions: "restart pause reverse pause",
-                },
-                fill: 'purple'
+                    onEnter: () => setToggle(true),
+                    onLeave: () => setToggle(false),
+                    // onLeaveBack: () => setColor(props.fill || 'green'),
+                    onLeaveBack: () => setToggle(false),
+                    onEnterBack: () => setToggle(true)
+                }
             })
         }
     }, [])
     
     const shapes = data.map((d, i) => {
         return (
-            <path ref={ref} key={i}
+            <animated.path ref={ref} key={i}
                 d={path(d)}
                 // stroke={'green'}
-                fill={props.fill}
+                fill={color}
+                opacity={animatedProps.opacity}
             />
         )
     })
