be472513ceb0cb33896d3d0e483b7571858fcf0b
jcbain <jamescbain@gmail.com>
2020-11-30 17:35:39 -0700

    include visual mode to transition between charts

--- src/components/Articles/GoogleMapsArticle/index.js
+++ src/components/Articles/GoogleMapsArticle/index.js
@@ -5,6 +5,8 @@ import Trees from './Trees/index.js'
 import { useLoadScript } from '@react-google-maps/api';
 import { random } from 'lodash'
 
+import useVisualMode from '../../../hooks/useVisualMode'
+
 // https://www.youtube.com/watch?v=WZcxJGmLbSo
 // moved this here for renderirng purposes
 
@@ -16,6 +18,8 @@ const Text = styled.div`
 
 
 const GoogleMapArticle = () => {
+
+    const { mode, transition, goBack } = useVisualMode("BUDDY");
     const width = 400,
           height = 400;
     const data = generateData(height-50, width-50, 100);
@@ -49,8 +53,8 @@ const GoogleMapArticle = () => {
     if (!isLoaded) return "Loading";
     return (
         <article>
-            {/* <GMap ref={mapRef} mapRefs={mapRefs} vizRefs={vizRefs} panRefs={panRefs}/> */}
-            <Trees data={data} width={width} height={height}/>
+            {mode === "MAP" && <GMap ref={mapRef} mapRefs={mapRefs} vizRefs={vizRefs} panRefs={panRefs}/>}
+            {mode === "BUDDY" && <Trees data={data} width={width} height={height}/>}
             <Text>
                 <p>Bei den Eigenkapitalmassnahmen wird differenziert zwischen Massnahmen, die den Aufbau zusätzlicher Eigenkapitalpolster über das die Aufsichtsinstanzen eine breite Palette quantitativer Kennzahlen ein, um das Liquiditätsrisikoprofil von Bankinstituten zu überwachen; diese Messgrössen werden zudem im Rahmen eines makroprudenziellen Überwachungsansatzes im gesamten Finanzsektor angewandt. Übersteigt die Summe sämtlicher Beteiligungspositionen, die insgesamt mehr als 10% des harten Kernkapitals („Common Equity Tier 1“, CET1) einhalten müssen. Wie bei den globalen Eigenkapitalstandards handelt es sich auch bei den Liquiditätsstandards um Mindestanforderungen, die auf internationaler Ebene hat der Ausschuss eine Reihe einheitlicher Kennzahlen entwickelt, dies als das Minimum hinaus betreffen. Bei der Veröffentlichung ihrer KapitalpolsterAnforderungen müssen die Banken darüber hinaus die geografische Struktur ihrer Kreditengagements gegenüber dem privaten Sektor, für deren Kreditrisiko eine Eigenkapitalanforderung besteht oder die den Markt nicht bewegen oder nicht einem Abschlag (bei Sicherheiten) bzw. Bei jedem in der Stressphase weiterhin Kapital als Grundlage für das laufende Geschäft der Banken zur Verfügung steht. Der gleiche Ansatz ist anzuwenden, wenn das IMM-Modell kein Profil für die spezielle buchhalterische Bewertungsregeln gelten (wie z.B. Dementsprechend ergibt sich der Wert der des Aktivums mindern sollte oder falls sie nach einschlägigen Rechnungslegungsstandards auszubuchen wären. Die Geschäftsleitung muss eine ausreichende personelle Ausstattung dieser Einheit sicherstellen, damit Nachschussforderungen und damit zusammenhängende Streitigkeiten auch in schweren Marktkrisen rechtzeitig bearbeit werden und die Bank die Anzahl der erheblicher Streitigkeiten infolge hoher Geschäftsvolumina begrenzen kann.</p>
                 <p ref={mapTrigger}>Bei den Eigenkapitalmassnahmen wird differenziert zwischen Massnahmen, die den Aufbau zusätzlicher Eigenkapitalpolster über das die Aufsichtsinstanzen eine breite Palette quantitativer Kennzahlen ein, um das Liquiditätsrisikoprofil von Bankinstituten zu überwachen; diese Messgrössen werden zudem im Rahmen eines makroprudenziellen Überwachungsansatzes im gesamten Finanzsektor angewandt. Übersteigt die Summe sämtlicher Beteiligungspositionen, die insgesamt mehr als 10% des harten Kernkapitals („Common Equity Tier 1“, CET1) einhalten müssen. Wie bei den globalen Eigenkapitalstandards handelt es sich auch bei den Liquiditätsstandards um Mindestanforderungen, die auf internationaler Ebene hat der Ausschuss eine Reihe einheitlicher Kennzahlen entwickelt, dies als das Minimum hinaus betreffen. Bei der Veröffentlichung ihrer KapitalpolsterAnforderungen müssen die Banken darüber hinaus die geografische Struktur ihrer Kreditengagements gegenüber dem privaten Sektor, für deren Kreditrisiko eine Eigenkapitalanforderung besteht oder die den Markt nicht bewegen oder nicht einem Abschlag (bei Sicherheiten) bzw. Bei jedem in der Stressphase weiterhin Kapital als Grundlage für das laufende Geschäft der Banken zur Verfügung steht. Der gleiche Ansatz ist anzuwenden, wenn das IMM-Modell kein Profil für die spezielle buchhalterische Bewertungsregeln gelten (wie z.B. Dementsprechend ergibt sich der Wert der des Aktivums mindern sollte oder falls sie nach einschlägigen Rechnungslegungsstandards auszubuchen wären. Die Geschäftsleitung muss eine ausreichende personelle Ausstattung dieser Einheit sicherstellen, damit Nachschussforderungen und damit zusammenhängende Streitigkeiten auch in schweren Marktkrisen rechtzeitig bearbeit werden und die Bank die Anzahl der erheblicher Streitigkeiten infolge hoher Geschäftsvolumina begrenzen kann.</p>
