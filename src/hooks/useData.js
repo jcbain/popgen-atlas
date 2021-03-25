f39aae5d624a6112d21f8f7ee194dfad7f9dcef9
jcbain <jamescbain@gmail.com>
2021-03-25 14:54:45 -0600

    remove prop dependency from chart selection

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,13 +1,8 @@
 import { useState, useEffect } from 'react';
-import { tidy, summarize, sum, groupBy, mutate } from '@tidyjs/tidy'
 
 import mutations from '../data/genome_data.json'
 
-const defaultParams = ['m', 'mu', 'sigsqr', 'pop',  'r'];
-
-
-
-const useData = (colorVar) => {
+const useData = () => {
     const [ data, setData ] = useState({})
     const [ loaded, setLoaded ] = useState(false)
 
@@ -16,41 +11,10 @@ const useData = (colorVar) => {
         setLoaded(true)
 
     }, [])
-    // const [gene, setGene] = useState([])
-    // const [ geneLoaded, setGeneLoaded ] = useState(false)
-    // const [phen, setPhen] = useState([])
-    // const [phenLoaded, setPhenLoaded] = useState(false)
-    // const allKeys = Object.keys(genes)
-    // const [tempParam, setTempParam] = useState(allKeys[0])
-    
-
-
-    // useEffect(() => {
-    //     setGene(genes[tempParam])
-    //     setGeneLoaded(true)
-    // }, [tempParam])
-
-    // useEffect(() => {  
-    //     const grouped = tidy(genes[tempParam], 
-    //         groupBy(['m', 'mu', 'sigsqr', 'pop', 'output_gen', 'r'], [summarize({ phen_diff: sum(colorVar) })]),
-    //         mutate({ 'phen_diff': d => d.phen_diff * 2})
-    //     )
-    //     setPhen(grouped)
-    //     setPhenLoaded(true)
-
-    // }, [tempParam])
-    
-
-    // const changeParams = () => {
-    //     setTempParam(allKeys[3])
-    // }
-
+  
     return { 
         data, 
         loaded
-        // phen, phenLoaded,
-        // gene, geneLoaded,
-        // changeParams 
     }
 }
 
--- src/hooks/useFilteredData.js
+++ src/hooks/useFilteredData.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { tidy, summarize, sum, groupBy, mutate } from '@tidyjs/tidy'
 
+const defaultParams = ['m', 'mu', 'sigsqr', 'pop',  'r'];
+const defaultGroupParams = ['m', 'mu', 'sigsqr', 'pop', 'output_gen', 'r']
 
-const useFilteredData = (data, loaded, colorVar, currentSet) =>  {
+const useFilteredData = (data, loaded, colorVar) =>  {
     const [ genes, setGenes ] = useState([]);
     const [ phens, setPhens ] = useState([]);
     const [ geneLoaded, setGeneLoaded ] = useState(false);
@@ -12,16 +14,16 @@ const useFilteredData = (data, loaded, colorVar, currentSet) =>  {
     console.log(Object.keys(data))
     useEffect(() => {
         if(loaded){
-            setGenes(data[currentSet])
+            setGenes(data[selectedParamSet])
             setGeneLoaded(true)
         }
         
-    }, [data, currentSet])
+    }, [data, selectedParamSet])
 
     useEffect(() => { 
         if(loaded){
-            const grouped = tidy(data[currentSet], 
-                groupBy(['m', 'mu', 'sigsqr', 'pop', 'output_gen', 'r'], [summarize({ phen_diff: sum(colorVar) })]),
+            const grouped = tidy(data[selectedParamSet], 
+                groupBy(defaultGroupParams, [summarize({ phen_diff: sum(colorVar) })]),
                 mutate({ 'phen_diff': d => d.phen_diff * 2})
             )
             setPhens(grouped)
@@ -30,7 +32,7 @@ const useFilteredData = (data, loaded, colorVar, currentSet) =>  {
         } 
        
 
-    }, [data, currentSet])
+    }, [data, selectedParamSet])
 
     return {
         genes, 
