06585c0dd8acc78b52ca4ea4e17d4965fe0bd8b6
jcbain <jamescbain@gmail.com>
2021-05-03 15:30:40 -0600

    develop a progress bar

--- src/components/inputs/PlayPauseReset.js
+++ src/components/inputs/PlayPauseReset.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Play, Pause, Reset } from '@styled-icons/boxicons-regular';
+import classnames from 'classnames';
 
 const Wrapper = styled.div`
-    width: 100px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 0.3fr 1fr;
+`
+
+const PlayResetWrapper = styled.div`
+    /* width: 100px; */
     display: grid;
     grid-template-columns: 1fr 1fr;
     column-gap: 10px;
@@ -15,8 +22,6 @@ const ButtonWrapper = styled.button`
     color: black;
     border-radius: 5px;
     width: 100%;
-    /* padding-top: 20px; */
-    /* padding-bottom: 20px; */
 `
 
 const StyledPlay = styled(Play)`
@@ -31,10 +36,51 @@ const StyledReset = styled(Reset)`
     width: 25px;
 `
 
+const ProgressWrapper = styled.div`
+    width: calc(100% - 40px);
+    padding-left: 20px;
+    /* padding-right: 20px; */
+    position: relative;
+`
+
+const Bar = styled.div`
+    position: absolute;
+    top: calc(50% - 2.5px);
+    background: lightgray;
+    height: 5px;
+    width: calc(100% - 20px);
+    border-radius: 2px;
+`
+
+const DotsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    position: absolute;
+    width: calc(100% - 20px);
+    top: calc(50% - 5px);
+    height: 10px;
+`
+
+const Dot = styled.div`
+    width: 10px;
+    border-radius: 50%;
+    background: lightgray;
+    &.dot-selected {
+        background: darkgray;
+    }
+`
+
 const PlayPauseReset = ({ resetter, script, stepInterval}) => {
 
     const [ play, setPlay ] = useState(false);
     const [ currentPoint, setCurrentPoint ] = useState(0);
+
+    const dots = script.map((s, i) => {
+        return(
+            <Dot key={i} className={classnames({'dot-selected': i  === currentPoint - 1})}/>
+        )
+    })
 
     const reset = () => {
         setCurrentPoint(0);
@@ -65,13 +111,22 @@ const PlayPauseReset = ({ resetter, script, stepInterval}) => {
 
     return (
         <Wrapper>
-            <ButtonWrapper onClick={() => setPlay(prev => !prev)}>
-                {play ? <StyledPause /> : <StyledPlay />}
-            </ButtonWrapper>
-            <ButtonWrapper onClick={reset}>
-                <StyledReset />
-            </ButtonWrapper>
+            <PlayResetWrapper>
+                <ButtonWrapper onClick={() => setPlay(prev => !prev)}>
+                    {play ? <StyledPause /> : <StyledPlay />}
+                </ButtonWrapper>
+                <ButtonWrapper onClick={reset}>
+                    <StyledReset />
+                </ButtonWrapper>
 
+            </PlayResetWrapper>
+            <ProgressWrapper>
+                <Bar />
+                <DotsContainer>
+                    {dots}
+                </DotsContainer>
+
+            </ProgressWrapper>
         </Wrapper>
     )
 }
