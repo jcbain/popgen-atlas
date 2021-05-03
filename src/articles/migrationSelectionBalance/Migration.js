605008effb3ffeae338823e1167fc189db5aca1f
jcbain <jamescbain@gmail.com>
2021-05-03 12:19:18 -0600

    design a play pause reset button

--- src/articles/migrationSelectionBalance/Migration.js
+++ src/articles/migrationSelectionBalance/Migration.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import classnames from 'classnames'
+
+import PlayPauseReset from '../../components/inputs/PlayPauseReset'
 
 import buddyRed from '../../images/buddy_red.png';
 import buddyBlue from '../../images/buddy_blue.png';
 import buddyPurple from '../../images/buddy_purple.png';
-import classNames from 'classnames';
 
 const buddies = { red: buddyRed, blue: buddyBlue, purple: buddyPurple };
 
@@ -13,6 +15,7 @@ const Wrapper = styled.div`
     height: 400px;
     margin-left: 20px;
     margin-right: 20px;
+    margin-bottom: 80px;
 `
 
 const DrawingArea = styled.div`
@@ -37,21 +40,86 @@ const LandDivider = styled.div`
 
 const BuddyImg = styled.img`
     position: absolute;
-    top: 10px;
+    top: ${({ topperc }) => topperc}%;
     left: ${({ leftperc }) => leftperc}%;
     width: 5%;
+    transition: all 0.5s;
+    &.transfer{
+        top: ${({ transfertop }) => transfertop}%;
+        left: ${({ transferleft }) => transferleft}%;
+    }
+    &.disappear{
+        display: none;
+    }
 `
 
+const buddyPositions = [
+    {color: 'red', top: 10, left: 5},
+    {color: 'blue', top: 10, left: 90, transfer: true, transferTop: 25, transferLeft: 20},
+    {color: 'red', top: 20, left: 15},
+    {color: 'blue', top: 20, left: 80},
+    {color: 'red', top: 10, left: 25},
+    {color: 'blue', top: 10, left: 70},
+    {color: 'red', top: 20, left: 35},
+    {color: 'blue', top: 20, left: 60},
+    {color: 'red', top: 30, left: 5},
+    {color: 'blue', top: 30, left: 90},
+    {color: 'red', top: 40, left: 15},
+    {color: 'blue', top: 40, left: 80},
+    {color: 'red', top: 30, left: 25},
+    {color: 'blue', top: 30, left: 70},
+    {color: 'red', top: 40, left: 35},
+    {color: 'blue', top: 40, left: 60},
+    {color: 'red', top: 50, left: 5},
+    {color: 'blue', top: 50, left: 90},
+    {color: 'red', top: 60, left: 15},
+    {color: 'blue', top: 60, left: 80},
+    {color: 'red', top: 50, left: 25},
+    {color: 'blue', top: 50, left: 70},
+    {color: 'red', top: 60, left: 35},
+    {color: 'blue', top: 60, left: 60},
+    {color: 'red', top: 70, left: 5},
+    {color: 'blue', top: 70, left: 90},
+    {color: 'red', top: 80, left: 15},
+    {color: 'blue', top: 80, left: 80},
+    {color: 'red', top: 70, left: 25},
+    {color: 'blue', top: 70, left: 70},
+    {color: 'red', top: 80, left: 35},
+    {color: 'blue', top: 80, left: 60}
+]
+
 const Migration = ({}) => {
+    const [ migrate, setMigrate ] = useState(true)
+    const [ die, setDie ] = useState(false);
+    const [ positionModified, setPositionModified ] = useState(false);
+    const [counter, setCounter] = useState(0)
+
+
+    const buds = buddyPositions.map((b, i) => {
+        return (
+            <BuddyImg key={i}
+                className={classnames({'transfer': b.transfer && migrate, 'disappear': die})}
+                src={b.color === 'red' ? buddies.red : buddies.blue}
+                leftperc={b.left}
+                topperc={b.top}
+                transfertop={b.transferTop}
+                transferleft={b.transferLeft}
+            />
+        )
+    })
+    
 
     return (
         <Wrapper>
             <DrawingArea>
                 <LandDivider />
                 <LandDivider className={classnames({'cold-side': true})}/>
-                <BuddyImg src={buddies.red} leftperc={10}/>
-                <BuddyImg src={buddies.blue} leftperc={90}/>
+                {buds}
+                {/* <BuddyImg src={buddies.red} leftperc={5} topperc={10}/>
+                <BuddyImg src={buddies.blue} leftperc={90} topperc={10}/> */}
             </DrawingArea>
+            <><PlayPauseReset updater={() => setCounter(prev => prev + 1)} resetter={() => setCounter(0)} /><p>{counter}</p></>
+            {/* <button onClick={() => setDie(prev => !prev)}>Click Me</button> */}
         </Wrapper>
     )
 }
---
+++ src/components/inputs/PlayPauseReset.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Play, Pause, Reset } from '@styled-icons/boxicons-regular';
+
+const Wrapper = styled.div`
+    width: 100px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 10px;
+`
+
+const ButtonWrapper = styled.button`
+    border: 2px solid black;
+    background-color: white;
+    color: black;
+    border-radius: 5px;
+    width: 100%;
+    /* padding-top: 20px; */
+    /* padding-bottom: 20px; */
+`
+
+const StyledPlay = styled(Play)`
+    width: 25px;
+`
+
+const StyledPause = styled(Pause)`
+    width: 25px;
+`
+
+const StyledReset = styled(Reset)`
+    width: 25px;
+`
+
+const PlayPauseReset = ({updater, resetter}) => {
+
+    const [ play, setPlay ] = useState(false);
+    const [ currentPoint, setCurrentPoint ] = useState(0);
+
+    const reset = () => {
+        setCurrentPoint(0);
+        resetter()
+        setPlay(false);
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (play) {
+            interval = setInterval(() => {
+                setCurrentPoint(s => s + 1);
+                updater()
+             }, 1000);
+            } else if (!play && currentPoint !== 0) {
+                clearInterval(interval);
+            }
+        return () => clearInterval(interval);
+    }, [play, currentPoint]);
+
+    return (
+        <Wrapper>
+            <ButtonWrapper onClick={() => setPlay(prev => !prev)}>
+                {play ? <StyledPause /> : <StyledPlay />}
+            </ButtonWrapper>
+            <ButtonWrapper onClick={reset}>
+                <StyledReset />
+            </ButtonWrapper>
+
+        </Wrapper>
+    )
+}
+
+export default PlayPauseReset;
