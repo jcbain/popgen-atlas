66392df1f8cfe8c6c2ec1f463718b8bcc9f9e93e
jcbain <jamescbain@gmail.com>
2021-05-26 16:10:48 -0600

    add animation flying capabilities

--- src/articles/migrationSelectionBalance/Map.js
+++ src/articles/migrationSelectionBalance/Map.js
@@ -37,76 +37,24 @@ const Map = () => {
     const [map, setMap] = useState(null)
     const [lng, setLng] = useState(-115.9);
     const [lat, setLat] = useState(51.35);
-    const [zoom, setZoom] = useState(11);
+    const [zoom, setZoom] = useState(8);
     const [active, setActive] = useState(false)
 
     useEffect(() => {
         // if (map.current) return; // initialize map only once
          const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/outdoors-v11',
+            style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
             center: [lng, lat],
             zoom: zoom,
             interactive: false,
-            pitch: 60, // pitch in degrees
-            bearing: -60, // bearing in degrees
+            pitch: 15, // pitch in degrees
+            bearing: -90, // bearing in degrees
         });
 
         map.on('load', () => {
-            map.addLayer({
-                "id": "countour-labels",
-                "type": "symbol",
-                "source": {
-                  type: 'vector',
-                  url: 'mapbox://mapbox.mapbox-terrain-v2'
-                },
-                "source-layer": "contour",
-                'layout': {
-                  'visibility': 'visible',
-                  'symbol-placement': 'line',
-                  'text-field': ['concat', ['to-string', ['get', 'ele']], 'm']
-                },
-                'paint': {
-                  'icon-color': '#877b59',
-                  'icon-halo-width': 1,
-                  'text-color': '#877b59',
-                  'text-halo-width': 1
-                }
-              })
-
-            //   map.addLayer({
-            //     "id": "countours",
-            //     "type": "line",
-            //     "source": {
-            //       type: 'vector',
-            //       url: 'mapbox://mapbox.mapbox-terrain-v2'
-            //     },
-            //     "source-layer": "contour",
-            //     'layout': {
-            //       'visibility': 'visible',
-            //       'line-join': 'round',
-            //       'line-cap': 'round'
-            //     },
-            //     'paint': {
-            //       'line-color': '#877b59',
-            //       'line-width': 1
-            //     }
-            //   })
-
-            setMap(map);
-        })
-
-        return () => map.remove();
-
-    }, [])
-
-    useEffect(() => {
-        move()
-    }, [active])
-
-    const move = () => {
-        if(map && active){
-            map.addSource('mapbox-dem', {
+      
+              map.addSource('mapbox-dem', {
                 'type': 'raster-dem',
                 'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
                 'tileSize': 512,
@@ -122,36 +70,30 @@ const Map = () => {
                 'paint': {
                 'sky-type': 'atmosphere',
                 'sky-atmosphere-sun': [0.0, 0.0],
-                'sky-atmosphere-sun-intensity': 15
+                'sky-atmosphere-sun-intensity': 12
                 }
                 });
-            // map.setPitchBearing(70, -70)
-            
-            // map.addSource('mapbox-dem', {
-            //     'type': 'raster-dem',
-            //     'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-            //     'tileSize': 512,
-            //     'maxzoom': 14
-            // });
-            // console.log(map)
-            // map.addLayer({
-            //     "id": "countours",
-            //     "type": "line",
-            //     "source": {
-            //       type: 'vector',
-            //       url: 'mapbox://mapbox.mapbox-terrain-v2'
-            //     },
-            //     "source-layer": "contour",
-            //     'layout': {
-            //       'visibility': 'visible',
-            //       'line-join': 'round',
-            //       'line-cap': 'round'
-            //     },
-            //     'paint': {
-            //       'line-color': '#877b59',
-            //       'line-width': 1
-            //     }
-            //   })
+
+
+            setMap(map);
+        })
+
+        return () => map.remove();
+
+    }, [])
+
+    useEffect(() => {
+        move()
+    }, [active])
+
+    const move = () => {
+        if(map && active){
+            map.flyTo({
+                center: [-117, 52], 
+                zoom: 11, 
+                pitch: 75
+            })
+
         }
     }
     
